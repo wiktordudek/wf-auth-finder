7ff2def35fada5f4bc09a303cb1e55fb05133546
Kamil Cyganowski <kamil.cyganowski@gmail.com>
2025-07-08 18:09:58 +0200

    improvements

--- app.py
+++ app.py
@@ -16,11 +16,10 @@ from exceptions import GameProcessNotFoundError, MemoryValueNotFoundError
 from log import CustomFormatter
 
 
-def find_game_process() -> psutil.Process:
+def find_game_process(name) -> psutil.Process:
     for process in psutil.process_iter():
-        if process.name() != PROCESS_NAME:
-            continue
-        return process
+        if process.name() == name:
+            return process
     raise GameProcessNotFoundError
 
 
@@ -28,19 +27,15 @@ def read_memory_value(process_pid: int, pattern: str, length: int) -> bytes:
     pattern_bytes = pattern.encode("utf-8")
     with OpenProcess(pid=process_pid) as process:
         for address in process.search_by_value(
-            pytype=str,
-            bufflength=len(pattern_bytes),
-            value=pattern_bytes,
+            pytype=str, bufflength=len(pattern_bytes), value=pattern_bytes
         ):
             return process.read_process_memory(
-                address + len(pattern_bytes),
-                bytes,
-                length,
+                address + len(pattern_bytes), bytes, length
             )
-    return MemoryValueNotFoundError(pattern)
+    raise MemoryValueNotFoundError(pattern)
 
 
-def setup_logger() -> None:
+def setup_logger() -> logging.Logger:
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     color_handler = logging.StreamHandler()
@@ -52,34 +47,37 @@ def setup_logger() -> None:
 if __name__ == "__main__":
     logger = setup_logger()
 
+    logger.info("🚀 Starting...")
     try:
-        logger.info("🚀 Starting...")
-
-        game_process = find_game_process()
+        game_process = find_game_process(PROCESS_NAME)
         game_pid = game_process.pid
-        logger.info(f"🎮 Found the game PID: {game_pid}")
-
-        nonce_data = read_memory_value(
-            game_pid,
-            NONCE_PATTERN,
-            NONCE_EXPECTED_LENGTH,
-        )
-
-        account_id_data = read_memory_value(
-            game_pid,
-            ACCOUNT_ID_PATTERN,
-            ACCOUNT_ID_EXPECTED_LENGTH,
-        )
-
-        data = {
-            "accountId": account_id_data.decode(),
-            "nonce": nonce_data.decode(),
-        }
-        logger.info("✅ Data extracted successfully:")
-        print(json.dumps(data, indent=4))
     except GameProcessNotFoundError:
         logger.error("❌ Couldn't find the game process.")
-    except MemoryValueNotFoundError as exception:
-        logger.error(f"❌ Couldn't find the {exception.pattern} value.")
+        raise
+    except MemoryValueNotFoundError as e:
+        logger.error(f"❌ Couldn't find the {e.pattern} value.")
+        raise
     except Exception:
         logger.exception("❌ Unexpected error occurred")
+        raise
+
+    logger.info(f"🎮 Found the game PID: {game_pid}")
+
+    nonce_data = read_memory_value(
+        game_pid,
+        NONCE_PATTERN,
+        NONCE_EXPECTED_LENGTH,
+    )
+
+    account_id_data = read_memory_value(
+        game_pid,
+        ACCOUNT_ID_PATTERN,
+        ACCOUNT_ID_EXPECTED_LENGTH,
+    )
+
+    data = {
+        "accountId": account_id_data.decode(),
+        "nonce": nonce_data.decode(),
+    }
+    logger.info("✅ Data extracted successfully:")
+    print(json.dumps(data, indent=4))
